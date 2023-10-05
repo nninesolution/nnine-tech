f955912adb0833640d7370bcb6c3f91bed3950f5
Deepak Bomjan <dpaktamang@gmail.com>
2023-10-05 10:23:05 +0545

    update texts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,12 +14,12 @@ const Footer = () => {
                     {/* 1st block */}
                     <div className="col-span-12 lg:col-span-4">
                         <div className="box-border border-b-4 border-blue-900 p-8 bg-gray-200 text-gray-600 text-center rounded-lg xl:w-80 mx-auto">
-                            <h3 className="font-bold text-4xl mb-4">MLD</h3>
+                            <h3 className="font-bold text-4xl mb-4">NNINE</h3>
                             <div className='text-md font-medium text-gray-600'>
-                                <h5>Molad e Konsult</h5>
-                                <p>Ilo Awela,</p>
-                                <p>Ota,</p>
-                                <p>Ogun State.</p>
+                                <h5>SundariJal</h5>
+                                <p>Kathamndu,</p>
+                                <p>Nepal,</p>
+                                <p>Bagmati State.</p>
                             </div>
                         </div>
                     </div>
@@ -98,7 +98,7 @@ const Footer = () => {
                         to="#"
                         className=" hover:text-gray-900"
                     >
-                        Molad e Konsult
+                        NNINE Solutions
                     </HashLink>. All rights reserved.
                     </div>
                 </div>
