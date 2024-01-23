3816dacc5bc70fc1e0b769f6ab76e40ca3531631
Niresh Dhakal <niresh44dhakal@gmail.com>
2024-01-23 23:58:59 +0545

    eslintrc updated

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -5,6 +5,9 @@ import { HashLink } from 'react-router-hash-link';
 const NavLinks = () => {
     return (
         <>
+            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
+                Demo our products
+            </Link>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#about">
                 About
             </HashLink>
@@ -17,8 +20,8 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact">
                 Contact Us
             </HashLink>
-            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
-                Demo our products
+            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/#signin">
+                Sign In
             </Link>
         </>
     )
