146a3691b60060cdfe7e12a8843483640f3d768d
nireshdhakal44 <39344465+nireshdhakal44@users.noreply.github.com>
2024-02-13 12:51:12 +0545

    first round modification done

---
+++ src/components/ServiceCard.js
@@ -0,0 +1,13 @@
+// ServiceCard.js
+import React from 'react';
+
+const ServiceCard = () => {
+    return (
+        <div className="bg-gradient-to-br from-green-400 to-blue-500 rounded-lg p-6 mb-6 shadow-md">
+            <h2 className="text-3xl font-semibold text-white mb-4">IT Training and Consultancy Services</h2>
+            <p className="text-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod ipsum vitae velit egestas, sed ultricies enim blandit. Vivamus vehicula, sapien sit amet volutpat tristique.</p>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/components/TrainersProfile.js
+++ src/components/TrainersProfile.js
@@ -1,10 +1,29 @@
-// TrainerProfile.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const TrainerProfile = (props) => {
+  const [isImageClicked, setIsImageClicked] = useState(false);
+
+  const handleImageClick = () => {
+    setIsImageClicked(!isImageClicked);
+  };
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-4 my-8">
-      <img src={props.imageUrl} alt={props.imageAlt} className="w-full" />
+    <div className="max-w-xs rounded overflow-hidden shadow-lg">
+      <img
+        src={props.imageUrl}
+        alt={props.imageAlt}
+        className="w-full h-auto cursor-pointer"
+        style={{ height: '200px' }}
+        onClick={handleImageClick}
+      />
+      {isImageClicked && (
+        <div
+          className="fixed top-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-900 bg-opacity-75"
+          onClick={handleImageClick}
+        >
+          <img src={props.imageUrl} alt={props.imageAlt} className="max-w-full max-h-full" />
+        </div>
+      )}
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{props.name}</div>
         <p className="text-gray-700 text-base">{props.description}</p>
