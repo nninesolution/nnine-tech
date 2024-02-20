7a5eaa45b1cabe7eee491086e990406378db2de4
nireshdhakal44 <39344465+nireshdhakal44@users.noreply.github.com>
2024-02-20 17:40:18 +0545

    trainers profile

--- src/components/TrainersProfile.js
+++ src/components/TrainersProfile.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 const TrainerProfile = (props) => {
-  const [isImageClicked, setIsImageClicked] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleImageClick = () => {
-    setIsImageClicked(!isImageClicked);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   return (
@@ -14,19 +14,28 @@ const TrainerProfile = (props) => {
         alt={props.imageAlt}
         className="w-full h-auto cursor-pointer"
         style={{ height: '200px' }}
-        onClick={handleImageClick}
+        onClick={toggleModal}
       />
-      {isImageClicked && (
-        <div
-          className="fixed top-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-900 bg-opacity-75"
-          onClick={handleImageClick}
-        >
-          <img src={props.imageUrl} alt={props.imageAlt} className="max-w-full max-h-full" />
+      {isModalOpen && (
+        <div className="fixed top-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-900 bg-opacity-75" onClick={toggleModal}>
+          <div className="max-w-md bg-white rounded-lg p-8">
+            <img src={props.imageUrl} alt={props.imageAlt} className="w-full h-auto mb-4" />
+            <div className="font-bold text-xl mb-4">{props.name}</div>
+            <p className="text-blue-900 text-base">{props.description}</p>
+          </div>
         </div>
       )}
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{props.name}</div>
-        <p className="text-gray-700 text-base">{props.description}</p>
+        <p className="text-blue-900 text-base text-justify line-clamp-5">{props.description}</p>
+        {!isModalOpen && (
+          <button
+            onClick={toggleModal}
+            className="text-blue-600 hover:text-blue-800 focus:outline-none"
+          >
+            See More
+          </button>
+        )}
       </div>
     </div>
   );
