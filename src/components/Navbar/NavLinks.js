dff83f4bd55d44de9fc283f09bdb52e7537a8aef
nireshdhakal44 <39344465+nireshdhakal44@users.noreply.github.com>
2024-02-20 08:52:30 +0545

    35 minor corrections (#37)
    * added pictures and some corrections
    
    * Map added and course timing updated

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -10,7 +10,7 @@ const NavLinks = () => {
             <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" to="/about">
                 About
             </Link>
-            <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" target='_blank' to="#">
+            <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" to="#">
                 Trainings
             </Link>
             <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" to="/courselist">
@@ -19,7 +19,8 @@ const NavLinks = () => {
             <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" to="/trainers">
                 Trainers
             </Link>
-            <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" target='_blank' to="#">
+            <Link className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl" to="#">
+
                 Blogs
             </Link>
         </>
