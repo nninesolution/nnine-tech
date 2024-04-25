75130ba89c054be6096a7c07578625fce2ee8a65
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-04-25 16:39:33 +0545

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter> {/* Wrap your entire app */}
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
