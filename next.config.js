adf90144a1ab92b3eb3fae4e8e7faeb684a9a4bd
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-04-28 14:35:25 +0545

    Feat: Add new

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+
+module.exports = {
+    // Specify the output directory for the Next.js build
+    distDir: 'build', // This is the output directory for Next.js <= 9.x
+  
+    // If you are using Next.js 10.x or later, use the following:
+    // This specifies the output directory for the production build
+    // The development build output directory is always ".next"
+    // output: 'build',
+  
+    // Other Next.js configuration options can be added here
+  };
+  
